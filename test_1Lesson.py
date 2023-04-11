c4b70e2a5e5b9c1c7f4946e75e507f16360dac9f
yakov_monarch <dmitry97yakovlev@mail.ru>
2023-04-11 21:39:43 +0300

    2 ДЗ 2

--- test_1Lesson.py
+++ test_1Lesson.py
@@ -48,3 +48,8 @@ def open_browser(size_window):
 def test_successful_login(open_browser):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_fail_search(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('qwertasdfgewwwwwwasd').press_enter()
+    browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
